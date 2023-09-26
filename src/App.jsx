aed91da24d93a0eac25f3daeef7152e5feed42eb
jforjanice <jjanice.cheng@gmail.com>
2023-09-26 16:34:10 +0200

    added questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Header } from "./Header.jsx";
+import { Questions } from "./Questions.jsx";
 
 export const App = () => {
   return (
     <div>
       <Header />
-      Survey Project test
+      <Questions />
     </div>
   );
 };
--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,5 +1,11 @@
-import { useState } from "react";
-
 export const Questions = () => {
-  return <div>Questions</div>;
+  return (
+    <div>
+      <label>How many cats do you have?</label>
+      <input></input>
+      <br />
+      <label>What is/are your cat(s) name?</label>
+      <input></input>
+    </div>
+  );
 };
