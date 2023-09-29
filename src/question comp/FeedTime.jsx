2e5fd1b2cfb8664525c870ba5596df1accbecf55
jforjanice <jjanice.cheng@gmail.com>
2023-09-29 11:03:33 +0200

    updated feed time value

--- src/question comp/FeedTime.jsx
+++ src/question comp/FeedTime.jsx
@@ -2,18 +2,26 @@ import React, { useState } from "react";
 
 export const FeedTime = ({ updateAnswer }) => {
   const handleChange = (event) => {
-    updateAnswer('feedtime', event.target.value);
-  }
+    updateAnswer("feedtime", event.target.value);
+  };
   return (
     <div className="question3">
-      <label className="feedTime">How many times do you want your cat to be fed?</label>
+      <label className="feedTime">
+        How many times do you want your cat to be fed?
+      </label>
       <br />
       <div className="Answers3">
-        <select className="feedTimeSelect" id="feedtime" onChange={handleChange}>
-          <option value="once">Morning </option>
-          <option value="twice"> Morning & evening</option>
-          <option value="threeTimes">Morning, lunch and evening</option>
-          <option value="fourTimes">
+        <select
+          className="feedTimeSelect"
+          id="feedtime"
+          onChange={handleChange}
+        >
+          <option value="Morning">Morning </option>
+          <option value="Morning & evening"> Morning & evening</option>
+          <option value="Morning, lunch and evenings">
+            Morning, lunch and evening
+          </option>
+          <option value="Morning, lunch and evening and before sleep">
             Morning, lunch and evening and before sleep
           </option>
         </select>
